b5f1307f76433f9fab188f96e1d8c595b725db94
Harshil <200456136@student.georgianc.on.ca>
2021-10-11 03:51:18 -0400

    Entered Provinces link to the Menu bar and added the provinces by ViewBag

---
+++ TravelO/Controllers/ProvincesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelO.Models;
+
+namespace TravelO.Controllers
+{
+    public class ProvincesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var provinces = new List<Province>();
+
+            for(var i=1; i<7; i++)
+            {
+                provinces.Add(new Province() { ProvinceID = i, Name = "Province " + i.ToString() });
+            }
+
+            return View(provinces);
+        }
+
+        //Browse the provinces
+        public IActionResult Browse(String province)
+        {
+
+            if(province == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.province = province;
+
+            return View();
+        }
+    }
+}
---
+++ TravelO/Models/Province.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelO.Models
+{
+    public class Province
+    {
+        public int ProvinceID { get; set; }
+        public String Name { get; set; }
+    }
+}
